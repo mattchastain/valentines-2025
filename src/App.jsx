58cbf4a177273882882f29ae726e20b8120f9872
Matt Chastain <work@mattchastain.dev>
2025-02-17 13:29:01 -0500

    add device detection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { isMobile } from 'react-device-detect';
 import './App.css';
 
 const music = new Audio('./audio/music.mp3');
@@ -30,6 +31,12 @@ function App() {
 				85
 			)}vw, ${getRandomNumber(5, 85)}vh)`,
 		});
+	};
+
+	const handleMouseEnter = () => {
+		if (!isMobile) {
+			handleSetPosition();
+		}
 	};
 
 	const [image, setImage] = useState('./images/CorgiHoldingFlowers.jpg');
@@ -68,7 +75,7 @@ function App() {
 					<button
 						className='no-btn'
 						onClick={handleNoClick}
-						// onMouseEnter={handleSetPosition}
+						onMouseEnter={handleMouseEnter}
 						style={moveBtn ? position : { top: 0, left: 0 }}
 					>
 						No
@@ -83,7 +90,7 @@ function App() {
 				<div className='letter'>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
-						class='envelope bi bi-envelope'
+						className='envelope bi bi-envelope'
 						viewBox='0 0 16 16'
 					>
 						<path d='M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z' />
