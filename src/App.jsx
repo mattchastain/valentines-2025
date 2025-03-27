c394c52f6708a76a0f6fee866633125e70fa9348
Matt Chastain <work@mattchastain.dev>
2025-03-27 13:14:23 -0400

    make buttons accessible

--- src/App.jsx
+++ src/App.jsx
@@ -67,13 +67,14 @@ function App() {
 			<div className='container'>
 				<h1>Happy Valentines Day!</h1>
 				<h3>Will you be my Valentine?</h3>
-				<img src={image} alt='' />
+				<img src={image} alt='image of corgi' />
 				<div className='options'>
-					<button className='yes-btn' onClick={handleYesClick}>
+					<button className='yes-btn' aria-label='click yes' onClick={handleYesClick}>
 						Yes
 					</button>
 					<button
 						className='no-btn'
+					    aria-label='click no'
 						onClick={handleNoClick}
 						onMouseEnter={handleMouseEnter}
 						style={moveBtn ? position : { top: 0, left: 0 }}
@@ -96,7 +97,7 @@ function App() {
 						<path d='M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z' />
 					</svg>
 				</div>
-				<button onClick={handleOpen}>Open</button>
+				<button aria-label='click to open envelope' onClick={handleOpen}>Open</button>
 			</>
 		);
 	}
